5fa82d0c6f3966cffa754c8383724998da3aee43
Neha Sacher <nehasacher143@gmail.com>
2018-12-19 19:49:03 +0530

    Tried Cart Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,13 +17,52 @@ function loadJSON(callback) {
   xobj.send(null);  
 }
 loadJSON(function(json) {
-  console.log(json);
+  // console.log(json);
  $('#resultData').html('');
   $.each(json.pizzas, function(key,value){
-      $('#resultData').append('<tr><td>'+value.pizza+'</td><td>'+value.crust+'</td><td>'+value.toppings+'</td><td>'+value.price+'</td><td>'+'<button>Add to Cart</button>'+'</td></tr>');
+    console.log(key+1);
+      $('#resultData').append('<tr><td>'+value.pizza+'</td><td>'+value.crust+'</td><td>'+value.toppings+'</td><td>'+value.price+'</td><td>'+'<button class="btn btn-danger my-cart-btn" data-id="' +(key+1)+ '" data-name="'+
+        value.pizza+'" data-price="'+ value.price +'" data-quantity="1">Add to Cart</button>'+'</td></tr>');
   });
 
 });
+
+
+
+
+var goToCartIcon = function($addTocartBtn){
+    var $cartIcon = $(".my-cart-icon");
+    var $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
+    $addTocartBtn.prepend($image);
+    var position = $cartIcon.position();
+    $image.animate({
+      top: position.top,
+      left: position.left
+    }, 500 , "linear", function() {
+      $image.remove();
+    });
+  }
+
+  $('.my-cart-btn').myCart({
+    classCartIcon: 'my-cart-icon',
+    classCartBadge: 'my-cart-badge',
+    affixCartIcon: true,
+    checkoutCart: function(products) {
+      $.each(products, function(){
+        console.log(this);
+      });
+    },
+    clickOnAddToCart: function($addTocart){
+      goToCartIcon($addTocart);
+    },
+    getDiscountPrice: function(products) {
+      var total = 0;
+      $.each(products, function(){
+        total += this.quantity * this.price;
+      });
+      return total;
+    }
+  });
 });
 
 
